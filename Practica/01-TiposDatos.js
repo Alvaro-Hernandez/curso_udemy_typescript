abcb9db6fe044f5e5a55618cb8789e79a3dea72b
Alvaro-Hernandez <alvaropineda606@gmail.com>
2024-01-23 23:57:54 -0600

    Tipos de Datos

--- Practica/01-TiposDatos.js
+++ Practica/01-TiposDatos.js
@@ -16,7 +16,7 @@ var numero = 10;
 numero = 35;
 // String
 var nombre = "Alvaro";
-console.log(typeof nombre);
+// console.log(typeof nombre);
 // Any(Cualquier tipo)
 var variable = 10;
 // Al cambiar el valor a string nos deja hacer el cambio ya que el tipo es any por lo que no nos arroja un error
@@ -28,3 +28,47 @@ var lista = ["uno", "dos", "tres"];
 // La tupla nos permite definir el orden de los elementos y sus tipos
 // Al no poner los tipos de los elementos nos arroja un error
 var tupla = ["uno", 1, true];
+// Añadir elementos a la tupla - Deberia devolver un error pero no lo hace
+tupla.push("Nuevo");
+// console.table(tupla);
+// Al añadir un dato en el array es mas stricto, debe ser del mismo tipo o dara un error
+// Error: No se puede asignar un argumento de tipo "number" al parámetro de tipo "string"
+// lista.push(5);
+lista.push("cuatro");
+// console.table(lista);
+// Enumerables o ENUM
+// Los enumerables nos permiten definir los valores de los elementos de un enumerable
+var Color;
+(function (Color) {
+    Color[Color["Rojo"] = 1] = "Rojo";
+    Color[Color["Verde"] = 2] = "Verde";
+    Color[Color["Azul"] = 3] = "Azul";
+    Color[Color["Amarillo"] = 4] = "Amarillo";
+    Color[Color["Negro"] = 5] = "Negro";
+})(Color || (Color = {}));
+// Si no les pongo un valor a los enum los toma como 0, 1, 2, 3, 4, 5, pero si cambio este valor cambiara en
+// el enum al valor que pongamos
+// Los enumerables nos permiten saber la posicion de un elemento
+var posicion = Color.Amarillo;
+// console.log(posicion);
+// Void - No retorna nada
+// Cuando usamos void la usamos cuando no queremos retornar nada
+function saludar() {
+    console.log("Hola");
+}
+// saludar();
+// Never - No retorna nada nunca
+// Cuando usamos never la usamos cuando no queremos retornar nada nunca
+function error(frase) {
+    throw new Error(frase);
+}
+// error("No funciona");
+// Null y Undefined
+// El tipo de null y undefined son sub-tipos de any
+// Es Undifined cuando no se le ha asignado un valor
+// Es Null cuando se le ha asignado un valor
+// Su diferencia es que es null cuando no se le ha asignado un valor y es undefined cuando se le ha asignado un valor
+var variableNull = null;
+var variableUndefined = undefined;
+// console.log(variableNull);
+// console.log(variableUndefined);
--- Practica/01-TiposDatos.ts
+++ Practica/01-TiposDatos.ts
@@ -20,7 +20,7 @@ numero = 35;
 
 // String
 let nombre: string = "Alvaro";
-console.log(typeof nombre);
+// console.log(typeof nombre);
 
 // Any(Cualquier tipo)
 let variable: any = 10;
@@ -34,3 +34,52 @@ let lista: string[] = ["uno", "dos", "tres"];
 // La tupla nos permite definir el orden de los elementos y sus tipos
 // Al no poner los tipos de los elementos nos arroja un error
 let tupla: [string, number, boolean] = ["uno", 1, true];
+
+// Añadir elementos a la tupla - Deberia devolver un error pero no lo hace
+tupla.push("Nuevo");
+// console.table(tupla);
+
+// Al añadir un dato en el array es mas stricto, debe ser del mismo tipo o dara un error
+// Error: No se puede asignar un argumento de tipo "number" al parámetro de tipo "string"
+// lista.push(5);
+lista.push("cuatro");
+// console.table(lista);
+
+// Enumerables o ENUM
+// Los enumerables nos permiten definir los valores de los elementos de un enumerable
+enum Color {
+  Rojo = 1,
+  Verde = 2,
+  Azul = 3,
+  Amarillo = 4,
+  Negro = 5,
+}
+// Si no les pongo un valor a los enum los toma como 0, 1, 2, 3, 4, 5, pero si cambio este valor cambiara en
+// el enum al valor que pongamos
+// Los enumerables nos permiten saber la posicion de un elemento
+let posicion: number = Color.Amarillo;
+// console.log(posicion);
+
+// Void - No retorna nada
+// Cuando usamos void la usamos cuando no queremos retornar nada
+function saludar(): void {
+  console.log("Hola");
+}
+// saludar();
+
+// Never - No retorna nada nunca
+// Cuando usamos never la usamos cuando no queremos retornar nada nunca
+function error(frase: string): never {
+  throw new Error(frase);
+}
+// error("No funciona");
+
+// Null y Undefined
+// El tipo de null y undefined son sub-tipos de any
+// Es Undifined cuando no se le ha asignado un valor
+// Es Null cuando se le ha asignado un valor
+// Su diferencia es que es null cuando no se le ha asignado un valor y es undefined cuando se le ha asignado un valor
+let variableNull: null = null;
+let variableUndefined: undefined = undefined;
+// console.log(variableNull);
+// console.log(variableUndefined);
